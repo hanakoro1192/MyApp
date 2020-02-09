0cee93e749141322524f6941fefd5ae6bedd18ab
宇都宮靖之 <utsunomiyayasuyuki@utsunomiyayasuyukinoMacBook-Pro.local>
2020-02-08 21:12:10 -0800

    error

--- js/main.js
+++ js/main.js
@@ -1,69 +1,74 @@
 //即時関数
-(function() {
+(function(){
     'use strict';
 
     var ths = document.getElementsByTagName('th');
-    var sortOrder = 1; // 1:昇順、　-1:降順
+    var sortOrder = 1; //１：昇順、−１：降順
 
     function rebuildTbody(rows){
         var tbody = document.querySelector('tbody');
         var i;
-        while (tbody.firstChild) {
+        while(tbody.firstChild) {
             tbody.removeChild(tbody.firstChild);
         }
         for (i = 0; i < rows.length; i++){
-            tbody.appendChild(rows[i]);
+            tbody.appendChild(rows[j]);
         }
     }
 
     function updateClassName(th){
-    var k;
-    for(k = 0; k < ths.length; k++){
-        ths[k].className = '';
-    }
-    th.className = sortOrder === 1 ? 'asc' : 'desc';
+        var k;
+            for (k = 0; k < ths.length; k++){
+                ths[k].className = '';
+            }
+            th.className = sortOrder === 1 ? 'asc' : 'desc';
     }
 
-    function compare(a,b,col,type){
-        _a = _a.children[col].textContent;
-        _b = _b.children[col].textContent;
+    function compare(a, b, col, type){
+        var _a = a.children[col].textContent * 1;
+        var _b = b.children[col].textContent * 1;
         if(type === "number"){
-        _a = _a * 1;
-        _b = _b * 1;
-        }else if(type === "string"){
-        _a = _a.toLowerCase();
-        _b = _b.toLowerCase();
+         _a = _a * 1;
+         _b = _b * 1;
+        }else if (type === "string"){
+         _a = _a.toLowerCase();
+         _b = _b.toLowerCase();
         }
-        if(_a < _b){
+        if (_a < _b){
             return -1;
         }
-        if(_a > _b){
+        if (_a > _b){
             return 1;
         }
         return 0;
     }
 
     function sortRows(th){
-        var rows = Array.prototype.slice.call(document.querySelectorAll('tbody > tr')); //セレクタを使用することができる //NodeList
-    var col = th.cellIndex;
-    var type = th.dataset.type; //string number
-    rows.sort(function(a,b){ //tr,tr
-        return compare(a,b,col,type) * sortOrder;
-    });
-    return rows;
+        var rows = Array.prototype.slice.call(document.querySelectorAll('tbody > tr'));
+        var col = th.cellIndex;
+        var type = th.dataset.type; //string number
+
+        rows.sort(function(a, b){ //tr, tr
+            return compare(a, b, col, type) * sortOrder;
+        });
+        return rows;
     }
 
     function setup(){
         var i;
         for(i = 0; i < ths.length; i++){
             ths[i].addEventListener('click', function(){
-            var rows;
-            rows = sortRows(this);
-            rebuildTbody(rows);
-            updateClassName(this);
-            sortOrder *= -1;
-                });
-            }
+                var rows;
+                rows = sortRows(this);
+    
+                rebuildTbody(rows);
+                
+                updateClassName(this);
+                
+                sortOrder *= -1;
+    
+            });
+        }
     }
 
     setup();
